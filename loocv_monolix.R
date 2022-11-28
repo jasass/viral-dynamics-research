ec14183a5bb4c7bbd2d9f9ca3fbbc3bf34388737
jasass <42650827+jasass@users.noreply.github.com>
2022-11-28 17:36:36 -0500

    Add files via upload

--- loocv_monolix.R
+++ loocv_monolix.R
@@ -1,4 +1,5 @@
 # Load my usual libraries that I use in every R script
+# If running in Rstudio, Click session -> set working directory -> to source file location
 
 library(ggplot2)
 library(dplyr)
@@ -12,6 +13,7 @@ library(kableExtra)
 library(tidyverse)
 library(scales)
 library(gtools)
+library(utils)
 
 # Load lixoft connectors (This library has to be manually installed and is included with Monolix)
 library(lixoftConnectors)
@@ -23,12 +25,12 @@ colnames(LOOCV) <- c('Model', 'Virus', 'sq_error', 'iD')
 
 # Load in SHIV data
 origDataSHIV <-
-  read.csv(file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod.csv", header =
+  read.csv(file = "data/VOP PVL_Rebound_cw_lod.csv", header =
              TRUE, sep = ",")
-
+getActiveDocumentContext()
 for (i in 1:length(origDataSHIV$animal_id)) {
   # Load path that has all of my monolix files
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
+  demoPath = getwd()
   # Load the specific monolix file that I've used
   project <- paste0(demoPath, "Damped Gomperts - SHIV.mlxtran")
   # Load the project and run population parameter estimation, so R can actually interface with the project
@@ -40,10 +42,10 @@ for (i in 1:length(origDataSHIV$animal_id)) {
   crossvalidData <- origDataSHIV[-c(i),]
   removedData <- origDataSHIV[i,]
   #Write the new data frame with the removed data point to a new csv
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
   #get the path of the new data frame with the removed data point
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv"
+    "data/VOP PVL_Rebound_cw_lod_crossvalid.csv"
   #Load the original data set from the monolix file
   BaseData <- getData()
   #Replace the original data set with the dataset with the removed data point,
@@ -82,16 +84,16 @@ for (i in 1:length(origDataSHIV$animal_id)) {
 }
 #Rinse and repeat this step for all models and all data sets
 for (i in 1:length(origDataSHIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
+  demoPath = getwd()
   project <- paste0(demoPath, "Two Stage Decay - SHIV.mlxtran")
   loadProject(projectFile = project)
   runPopulationParameterEstimation()
   
   crossvalidData <- origDataSHIV[-c(i), ]
   removedData <- origDataSHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv"
+    "data/VOP PVL_Rebound_cw_lod_crossvalid.csv"
   BaseData <- getData()
   setData(filtereddatfile,
           BaseData$headerTypes,
@@ -119,16 +121,16 @@ for (i in 1:length(origDataSHIV$animal_id)) {
 }
 
 for (i in 1:length(origDataSHIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Perelson Decay - SHIV.mlxtran")
+  demoPath = getwd()
+  project <- paste0(demoPath, "Two Stage Decay New - SHIV.mlxtran")
   loadProject(projectFile = project)
   runPopulationParameterEstimation()
   
   crossvalidData <- origDataSHIV[-c(i), ]
   removedData <- origDataSHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv"
+    "data/VOP PVL_Rebound_cw_lod_crossvalid.csv"
   BaseData <- getData()
   setData(filtereddatfile,
           BaseData$headerTypes,
@@ -155,61 +157,26 @@ for (i in 1:length(origDataSHIV$animal_id)) {
   
   sq_error <- (log10(removedData$viral_load) - log10(vl)) ^ 2
   LOOCV[1 + length(LOOCV$Model), ] <-
-    c("PD", 'SHIV', as.numeric(sq_error), removedData$animal_id)
-}
-
-for (i in 1:length(origDataSHIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Two Stage Decay New - SHIV.mlxtran")
-  loadProject(projectFile = project)
-  runPopulationParameterEstimation()
-  
-  crossvalidData <- origDataSHIV[-c(i), ]
-  removedData <- origDataSHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv", row.names = FALSE)
-  filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/VOP PVL_Rebound_cw_lod_crossvalid.csv"
-  BaseData <- getData()
-  setData(filtereddatfile,
-          BaseData$headerTypes,
-          BaseData$observationTypes)
-  
-  runPopulationParameterEstimation()
-  runConditionalModeEstimation()
-  
-  crossvalidParams <-
-    getEstimatedIndividualParameters(method = "conditionalMode")
-  newParams = crossvalidParams$conditionalMode[crossvalidParams$conditionalMode$id == removedData$animal_id, ]
-  
-  V0 <- newParams$V0
-  delta <- newParams$delta
-  gamma <- newParams$gamma
-  r <- newParams$r
-  tau <- removedData$week_post_treatment
-  
-  vl <- V0 * r* exp(-delta*tau) + (1-r)*V0*exp(-gamma*tau)
-  sq_error <- (log10(removedData$viral_load) - log10(vl)) ^ 2
-  LOOCV[1 + length(LOOCV$Model), ] <-
-    c("TSDN", 'SHIV', as.numeric(sq_error), removedData$animal_id)
+    c("TSD", 'SHIV', as.numeric(sq_error), removedData$animal_id)
 }
 
 ## SIV Portion
 
 origDataSIV <-
-  read.csv(file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV.csv", header =
+  read.csv(file = "data/InfantR01 - SIV.csv", header =
              TRUE, sep = ",")
 
 for (i in 1:length(origDataSIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
+  demoPath = getwd()
   project <- paste0(demoPath, "Damped Gomperts - SIV.mlxtran")
   loadProject(projectFile = project)
   runPopulationParameterEstimation()
   
   crossvalidData <- origDataSIV[-c(i), ]
   removedData <- origDataSIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/InfantR01 - SIV_crossvalid.csv", row.names = FALSE)
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv"
+    "data/InfantR01 - SIV_crossvalid.csv"
   BaseData <- getData()
   setData(filtereddatfile,
           BaseData$headerTypes,
@@ -239,53 +206,16 @@ for (i in 1:length(origDataSIV$animal_id)) {
 }
 
 for (i in 1:length(origDataSIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Two Stage Decay - SIV.mlxtran")
+  demoPath = getwd()
+  project <- paste0(demoPath, "Two Stage Decay New - SIV.mlxtran")
   loadProject(projectFile = project)
   runPopulationParameterEstimation()
   
   crossvalidData <- origDataSIV[-c(i), ]
   removedData <- origDataSIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/InfantR01 - SIV_crossvalid.csv", row.names = FALSE)
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv"
-  BaseData <- getData()
-  setData(filtereddatfile,
-          BaseData$headerTypes,
-          BaseData$observationTypes)
-  
-  runPopulationParameterEstimation()
-  runConditionalModeEstimation()
-  
-  crossvalidParams <-
-    getEstimatedIndividualParameters(method = "conditionalMode")
-  newParams = crossvalidParams$conditionalMode[crossvalidParams$conditionalMode$id == removedData$animal_id, ]
-  
-  V0 <- newParams$V0
-  lambda1 <- newParams$lambda1
-  lambda2 <- newParams$lambda2
-  cp <- newParams$cp
-  tau <- removedData$weeks_post_treatment
-  
-  vl <-
-    1 * (tau <= cp) * V0 * exp(-lambda1 * (tau)) + 1 * (tau > cp) * V0 * exp(-lambda1 * (cp)) * exp(-lambda2 * (tau - cp))
-  
-  sq_error <- (log10(removedData$viral_load) - log10(vl)) ^ 2
-  LOOCV[1 + length(LOOCV$Model), ] <-
-    c("TSD", 'SIV', as.numeric(sq_error), removedData$animal_id)
-}
-
-for (i in 1:length(origDataSIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Perelson Decay - SIV.mlxtran")
-  loadProject(projectFile = project)
-  runPopulationParameterEstimation()
-  
-  crossvalidData <- origDataSIV[-c(i), ]
-  removedData <- origDataSIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv", row.names = FALSE)
-  filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv"
+    "data/InfantR01 - SIV_crossvalid.csv"
   BaseData <- getData()
   setData(filtereddatfile,
           BaseData$headerTypes,
@@ -312,60 +242,26 @@ for (i in 1:length(origDataSIV$animal_id)) {
   
   sq_error <- (log10(removedData$viral_load) - log10(vl)) ^ 2
   LOOCV[1 + length(LOOCV$Model), ] <-
-    c("PD", 'SIV', as.numeric(sq_error), removedData$animal_id)
+    c("TSD", 'SIV', as.numeric(sq_error), removedData$animal_id)
 }
 
-for (i in 1:length(origDataSIV$animal_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Two Stage Decay New - SIV.mlxtran")
-  loadProject(projectFile = project)
-  runPopulationParameterEstimation()
-  
-  crossvalidData <- origDataSIV[-c(i), ]
-  removedData <- origDataSIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv", row.names = FALSE)
-  filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/InfantR01 - SIV_crossvalid.csv"
-  BaseData <- getData()
-  setData(filtereddatfile,
-          BaseData$headerTypes,
-          BaseData$observationTypes)
-  
-  runPopulationParameterEstimation()
-  runConditionalModeEstimation()
-  
-  crossvalidParams <-
-    getEstimatedIndividualParameters(method = "conditionalMode")
-  newParams = crossvalidParams$conditionalMode[crossvalidParams$conditionalMode$id == removedData$animal_id, ]
-  
-  V0 <- newParams$V0
-  delta <- newParams$delta
-  gamma <- newParams$gamma
-  r <- newParams$r
-  tau <- removedData$weeks_post_treatment
-  
-  vl <- V0 * r* exp(-delta*tau) + (1-r)*V0*exp(-gamma*tau)
-  sq_error <- (log10(removedData$viral_load) - log10(vl)) ^ 2
-  LOOCV[1 + length(LOOCV$Model), ] <-
-    c("TSDN", 'SIV', as.numeric(sq_error), removedData$animal_id)
-}
 ## HIV Portion
 
 origDataHIV <-
-  read.csv(file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData.csv", header =
+  read.csv(file = "data/humanData.csv", header =
              TRUE, sep = ",")
 
 for (i in 1:length(origDataHIV$human_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
+  demoPath = getwd()
   project <- paste0(demoPath, "Damped Gomperts - HIV.mlxtran")
   loadProject(projectFile = project)
   runPopulationParameterEstimation()
   
   crossvalidData <- origDataHIV[-c(i), ]
   removedData <- origDataHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/humanData_crossvalid.csv", row.names = FALSE)
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv"
+    "data/humanData_crossvalid.csv"
   BaseData <- getData()
   setData(filtereddatfile,
           BaseData$headerTypes,
@@ -395,53 +291,16 @@ for (i in 1:length(origDataHIV$human_id)) {
 }
 
 for (i in 1:length(origDataHIV$human_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Two Stage Decay - HIV.mlxtran")
+  demoPath = getwd()
+  project <- paste0(demoPath, "Two Stage Decay New - HIV.mlxtran")
   loadProject(projectFile = project)
   runPopulationParameterEstimation()
   
   crossvalidData <- origDataHIV[-c(i), ]
   removedData <- origDataHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv", row.names = FALSE)
+  write.csv(crossvalidData, file = "data/humanData_crossvalid.csv", row.names = FALSE)
   filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv"
-  BaseData <- getData()
-  setData(filtereddatfile,
-          BaseData$headerTypes,
-          BaseData$observationTypes)
-  
-  runPopulationParameterEstimation()
-  runConditionalModeEstimation()
-  
-  crossvalidParams <-
-    getEstimatedIndividualParameters(method = "conditionalMode")
-  newParams = crossvalidParams$conditionalMode[crossvalidParams$conditionalMode$id == removedData$human_id, ]
-  
-  V0 <- newParams$V0
-  lambda1 <- newParams$lambda1
-  lambda2 <- newParams$lambda2
-  cp <- newParams$cp
-  tau <- removedData$time_relative
-  
-  vl <-
-    1 * (tau <= cp) * V0 * exp(-lambda1 * (tau)) + 1 * (tau > cp) * V0 * exp(-lambda1 * (cp)) * exp(-lambda2 * (tau - cp))
-  
-  sq_error <- (removedData$log_vLoad  - log10(vl)) ^ 2
-  LOOCV[1 + length(LOOCV$Model), ] <-
-    c("TSD", 'HIV', as.numeric(sq_error), removedData$human_id)
-}
-
-for (i in 1:length(origDataHIV$human_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Perelson Decay - HIV.mlxtran")
-  loadProject(projectFile = project)
-  runPopulationParameterEstimation()
-  
-  crossvalidData <- origDataHIV[-c(i), ]
-  removedData <- origDataHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv", row.names = FALSE)
-  filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv"
+    "data/humanData_crossvalid.csv"
   BaseData <- getData()
   setData(filtereddatfile,
           BaseData$headerTypes,
@@ -468,47 +327,12 @@ for (i in 1:length(origDataHIV$human_id)) {
   
   sq_error <- (removedData$log_vLoad  - log10(vl)) ^ 2
   LOOCV[1 + length(LOOCV$Model), ] <-
-    c("PD", 'HIV', as.numeric(sq_error), removedData$human_id)
-}
-
-for (i in 1:length(origDataHIV$human_id)) {
-  demoPath = 'C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/code/summer-2019/'
-  project <- paste0(demoPath, "Two Stage Decay New - HIV.mlxtran")
-  loadProject(projectFile = project)
-  runPopulationParameterEstimation()
-  
-  crossvalidData <- origDataHIV[-c(i), ]
-  removedData <- origDataHIV[i, ]
-  write.csv(crossvalidData, file = "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv", row.names = FALSE)
-  filtereddatfile <-
-    "C:/Users/scama/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/Humans/humanData_crossvalid.csv"
-  BaseData <- getData()
-  setData(filtereddatfile,
-          BaseData$headerTypes,
-          BaseData$observationTypes)
-  
-  runPopulationParameterEstimation()
-  runConditionalModeEstimation()
-  
-  crossvalidParams <-
-    getEstimatedIndividualParameters(method = "conditionalMode")
-  newParams = crossvalidParams$conditionalMode[crossvalidParams$conditionalMode$id == removedData$human_id, ]
-  
-  V0 <- newParams$V0
-  delta <- newParams$delta
-  gamma <- newParams$gamma
-  r <- newParams$r
-  tau <- removedData$time_relative
-  
-  vl <- V0 * r* exp(-delta*tau) + (1-r)*V0*exp(-gamma*tau)
-  sq_error <- (removedData$log_vLoad  - log10(vl)) ^ 2
-  LOOCV[1 + length(LOOCV$Model), ] <-
-    c("TSDN", 'HIV', as.numeric(sq_error), removedData$human_id)
+    c("TSD", 'HIV', as.numeric(sq_error), removedData$human_id)
 }
 
 #Finally, write the LOOCV data frame to a csv, including the standard error
 # for each data/model combination for use in plots.
-write.csv(LOOCV, file = "C:/Users/Julian/Dropbox/2019_Summer_Project_Rebound/Decay_Dynamics/crossvalidData.csv", row.names = FALSE)
+write.csv(LOOCV, file = "crossvalidData.csv", row.names = FALSE)
 cdata <- ddply(
   LOOCV,
   c("Model", "Virus"),
